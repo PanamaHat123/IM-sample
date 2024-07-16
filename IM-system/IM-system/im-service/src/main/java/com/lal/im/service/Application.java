ccf1e824d6e40628c22bc5722295221bf11123c7
余晓波 <461837619@qq.com>
2024-07-16 10:59:18 +0800

    rabbitmq redis connect

--- IM-system/IM-system/im-service/src/main/java/com/lal/im/service/Application.java
+++ IM-system/IM-system/im-service/src/main/java/com/lal/im/service/Application.java
@@ -1,34 +1,23 @@
-package com.lal.im.tcp;
+package com.lal.im.service;
+
 
 import com.rabbitmq.client.Channel;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 
 import java.util.Map;
 
-@Slf4j
 @SpringBootApplication
-public class TcpApplication {
-
-
-    static String port;
-
-    @Autowired(required = false)
-    @Value("${server.port}")
-    public void setPort(String port) {
-        TcpApplication.port = port;
-    }
+public class Application {
 
     /**
      * test rabbitmq
@@ -45,9 +34,7 @@ public class TcpApplication {
         System.out.println("receive message:"+message.toString());
     }
 
-    public static void main(String[] args) {
-        ApplicationContext applicationContext = SpringApplication.run(TcpApplication.class, args);
-        log.info("spring boot server is running on port {}...",port);
-
+        public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
     }
 }
